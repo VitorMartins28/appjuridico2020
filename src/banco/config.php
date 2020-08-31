c4d854eb965b71891e1fad82993e16c1e3e10115
VitorMartins28 <vitormartins2819992gmail.com>
2020-08-31 20:04:30 -0300

    tipo pocessos

---
+++ src/banco/config.php
@@ -0,0 +1,14 @@
+<?php
+
+ date_default_timezone_set('America/Sao_Paulo');
+
+ define('DBHOST', 'localhost');
+ define('DBHUSER' , 'root');
+ define ('DBPASSOWORD', '');
+ define ('DBNAME', 'db_appjuridico');
+
+ defie ('MSG_FALHA_CONEXAO', 'Ops... não foi possivel conectar ao banco de dados!');
+define('MSG_CAMPOS_OBRIGATORIOS','Existem campos obrigatorios');
+define('TP_MSG_ERROR', 'error');
+define('TP_MSG_SUCCESS', 'success');
+define('TP_MSG_INFO', 'info');
